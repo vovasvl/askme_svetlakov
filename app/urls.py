0524c4b9f32410500be900fc1de02f58eaec5409
Vladimir Svetlakov <vova.svetl@mail.ru>
2024-04-08 17:17:13 +0300

    Добавил все html, вынес пагинатор в отдельную функцию

--- app/urls.py
+++ app/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('hot/', views.hot, name='hot'),
     path('question/<int:question_id>', views.question, name='question'),
     path('ask/', views.ask, name='ask'),
-
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('settings/', views.settings, name='settings'),
+    path('tag/<str:tag_name>', views.tag, name='tag')
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
+from django.http import Http404
 from django.shortcuts import render
-
+from math import ceil
 # Create your views here.
 
 QUESTIONS = [
@@ -10,19 +11,58 @@ QUESTIONS = [
         "text": f"There is question number {i}"
     } for i in range(200)
 ]
+ANSWERS = [
+    {
+        "id": i,
+        "title": f"Question {i}",
+        "text": f"There is question number {i}"
+    } for i in range(200)
+]
+
+TAGS={"tagtag": {"id": 1, "questions": QUESTIONS[10:100]}, "lol": {"id": 2, "questions": QUESTIONS[100:195]}}
+
+def pagination(objects_list, request,per_page=5):
+    page_num = request.GET.get('page', 1)
+    try:
+        if (int(page_num)<1 or int(page_num) > ceil(len(objects_list) / per_page)):
+            raise Http404("That page contains no results (Wrong numbers, my man...)")
+    except ValueError:
+        raise Http404("That page contains no results (try typing integer page number, maybe)")
+    paginator = Paginator(objects_list, per_page)
+    page_obj = paginator.page(page_num)
+    return page_obj
 
 def index(request):
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 5)
-    page_obj = paginator.page(page_num)
-    return render(request, "index.html", {"questions": page_obj})
+
+    questions=pagination(QUESTIONS,request)
+    return render(request, "index.html", {"questions": questions})
 
 def hot(request):
-    return render(request, "hot_questions.html", {"questions": QUESTIONS[::-1]})
+
+    questions=pagination(QUESTIONS[::-1],request)
+
+    return render(request, "hot_questions.html", {"questions": questions})
 
 def question(request, question_id):
-    item = QUESTIONS[question_id]
-    return render(request, "question_detail.html", {"question": item})
+
+    question = QUESTIONS[question_id]
+    answers=pagination(ANSWERS,request)
+
+    return render(request, "question_detail.html", {"question": question, "answers": answers})
 
 def ask(request):
     return render(request, "ask.html")
+def login(request):
+    return render(request, "login.html")
+
+def signup(request):
+    return render(request, "signup.html")
+def settings(request):
+    return render(request, "settings.html")
+def tag(request, tag_name):
+    item = TAGS.get(tag_name)
+    if(item==None):
+        raise Http404('hello there, you seem lost')
+
+    questions = pagination(TAGS.get(tag_name).get('questions'),request)
+    return render(request, "tag.html", {"tag_name": tag_name, "questions": questions})
